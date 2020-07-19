d6430c8308fe598551b5b62b5ace4ea6d387b2cf
Ken Charette <charettekenneth@gmail.com>
2020-07-18 20:44:33 -0400

    Initial start of project with express server

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on Port 5000');
+});
