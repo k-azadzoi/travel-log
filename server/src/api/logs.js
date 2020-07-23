6771e99a54ff75ae71633c3375eb564be5f8377b
Ken Charette <charettekenneth@gmail.com>
2020-07-22 21:00:21 -0400

    Created route to api/logs to show list of travel entries

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -4,10 +4,13 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Checking if the routes work',
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
