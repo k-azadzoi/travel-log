48a4fcd0e0b23075a5f832cb0c1bc0ed022f3b09
Ken Charette <charettekenneth@gmail.com>
2020-07-23 19:02:18 -0400

    Updated MapBox style and added in function that makes a fetch to travel log API

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
+import { listLogEntries } from './API'
 
 const App = () => {
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh ',
-    latitude: 37.7577,
-    longitude: -122.4376,
+    center: [33.2232, 43.6793],
     zoom: 1
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      console.log(logEntries)
+    })()
+  },[])
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
