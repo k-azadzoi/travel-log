5d3b1eaa78683798d13912374b690ab065bf50ce
Ken Charette <charettekenneth@gmail.com>
2020-07-23 20:11:15 -0400

    Fetching from API of locations. Shows up on the map as marker

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 import { listLogEntries } from './API'
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh ',
-    center: [33.2232, 43.6793],
-    zoom: 1
+    latitude: 48.864716,
+    longitude: 2.349014, 
+    zoom: 1.75
   });
 
   useEffect(() => {
     (async () => {
       const logEntries = await listLogEntries();
-      console.log(logEntries)
+      setLogEntries(logEntries);
     })()
   },[])
 
@@ -24,7 +26,33 @@ const App = () => {
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      {logEntries.map(entry => (
+        <Marker 
+          key={entry._id}
+          latitude={entry.latitude} 
+          longitude={entry.longitude} 
+          offsetLeft={-12} 
+          offsetTop={-24}
+        >
+          <svg 
+            className="marker" 
+            style={{
+              width: '24px',
+              height: '24px'
+            }}
+            viewBox="0 0 24 24" 
+            stroke-width="1" 
+            fill='orange' 
+            stroke-linecap="round" 
+            stroke-linejoin="round"> 
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+            <circle cx="12" cy="10" r="3"></circle>
+          </svg>
+        </Marker>
+      ))
+      }
+    </ReactMapGL>
   );
 }
 
