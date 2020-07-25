9b918d7d4ae51396c8ce0ca8af68dae5823b9028
Ken Charette <charettekenneth@gmail.com>
2020-07-25 00:14:54 -0400

    Created LogEntryForm component with styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import { listLogEntries } from './API'
+import LogEntryForm from './LogEntryForm';
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
@@ -118,7 +119,7 @@ const App = () => {
                   anchor="top"
                 >
                   <div className="popup">
-                    <h3>Add your new log entry!</h3>
+                    <LogEntryForm/>
                   </div>
                 </Popup>
                 </>
