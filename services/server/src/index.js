bcb16080659b821a1dca013f7789ecda907fdf13
Ken Charette <charettekenneth@gmail.com>
2021-01-24 16:23:43 -0500

    Restructuring folders and adding a Dockerfile

---
+++ services/server/src/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+const app = express();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log(`Mongo connected on ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello world',
+  });
+});
+
+app.use('/api/logs', logs);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
